6a2a1d78571ba760843576f22f8c7add47e8f5a5
Max Deibel <deibel.max@gmail.com>
2022-05-24 20:58:39 -0700

    Add basic UI for rps game

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-    const randomChoice = Math.floor(Math.random()*3);
+    const randomChoice = Math.floor(Math.random() * 3);
     if (randomChoice === 0) {
         return 'Rock';
     }
@@ -13,7 +13,7 @@ function computerPlay() {
 
 function playOneRound(playerSelection, computerSelection) {
     const lowerComputerSelection = computerSelection.toLowerCase();
-    switch(playerSelection.toLowerCase()) {
+    switch (playerSelection.toLowerCase()) {
         case 'rock':
             if (lowerComputerSelection === 'rock') {
                 return 'It\'s a tie! Rock ties Rock.';
@@ -49,21 +49,39 @@ function playOneRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i ++) {
-        const playerSelection = prompt('Pick rock, paper, or scissors:');
-        const result = playOneRound(playerSelection, computerPlay());
-        if (result.startsWith('You win!')) {
-            playerScore++;
-        }
-        else if (result.startsWith('You lose!')) {
-            computerScore++;
-        }
-        console.log(result);
+function game(playerSelection) {
+    const result = playOneRound(playerSelection, computerPlay());
+    if (result.startsWith('You win!')) {
+        playerScore++;
     }
-    console.log(`Final score: You have ${playerScore}, computer has ${computerScore}`);
+    else if (result.startsWith('You lose!')) {
+        computerScore++;
+    }
+    const round = document.querySelector('.round');
+    round.textContent = result;
+    score.textContent = `You have ${playerScore}, computer has ${computerScore}.`;
+    if (playerScore >= 5) {
+        score.textContent += ' You won the match! Refresh to play again.'
+        disableButtons();
+    }
+    else if (computerScore >= 5) {
+        score.textContent += ' Computer has won the match! Refresh to play again.'
+        disableButtons();
+    }
 }
 
-game();
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => { game(button.textContent) });
+});
+
+let playerScore = 0;
+let computerScore = 0;
+const score = document.querySelector('.score');
+score.textContent = `You have ${playerScore}, computer has ${computerScore}`;
